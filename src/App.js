54616f6e226eae925b058cb5eadb1b81b8e54d6f
Michael Peter <michaelhpet@gmail.com>
2023-04-12 21:30:12 +0100

    chore: remove boilerplate code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css"
 
 function App() {
-  return <h1>Hello World</h1>
+  return (
+    <div className="main-container">
+      <h1 className="big-text">Hello Todo App</h1>
+      <button>Click Me</button>
+    </div>
+  )
 }
 
 export default App
