81cbf587ae7439bf9294b197822791bd9e02896e
Michael Peter <michaelhpet@gmail.com>
2023-04-14 21:30:34 +0100

    feat: create JSX markup for Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { FeaturedSection } from "./components/FeaturedSection"
+import { Footer } from "./components/Footer"
 import { Header } from "./components/Header"
 import { HeroSection } from "./components/HeroSection"
 import { LastSection } from "./components/LastSection"
@@ -11,6 +12,7 @@ function App() {
       <HeroSection />
       <FeaturedSection />
       <LastSection />
+      <Footer />
     </main>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,35 @@
+export function Footer() {
+  return (
+    <footer>
+      <div>
+        <article>
+          <img src="" alt="" />
+          <p>Lets change your habit join with million people</p>
+        </article>
+
+        <article>
+          <p>DK Tech Company</p>
+          <p>Uma Street, Lost City, Aincard </p>
+        </article>
+      </div>
+
+      <div>
+        <div>
+          <p>Features</p>
+          <nav>Lorem</nav>
+          <nav>Ipsum</nav>
+          <nav>Dolor</nav>
+          <nav>Sit</nav>
+        </div>
+
+        <div>
+          <p>Pricing</p>
+          <nav>Lorem</nav>
+          <nav>Ipsum</nav>
+          <nav>Dolor</nav>
+          <nav>Sit</nav>
+        </div>
+      </div>
+    </footer>
+  )
+}
