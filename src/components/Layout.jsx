a07d488099b09001656e0e95c2d89ca8596b1647
Michael Peter <michaelhpet@gmail.com>
2023-04-18 21:16:50 +0100

    feat: create markup for Layout header

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,18 @@
+import logo from "../images/logo-alt.png"
+import avatar from "../images/avatar.png"
+
 export function Layout(props) {
   return (
     <div>
-      <header></header>
+      <header>
+        <img src={logo} alt="Todo daily logo" />
+
+        <div>
+          <p>James</p>
+          <img src={avatar} alt="Profile avatar" />
+        </div>
+      </header>
+
       <div>
         <aside></aside>
         <main></main>
