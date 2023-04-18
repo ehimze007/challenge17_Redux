a5676541e0924b4e7436b5feac9822fe778faeaf
Michael Peter <michaelhpet@gmail.com>
2023-04-18 21:23:52 +0100

    feat: add styles for Layout header

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,14 @@
 import logo from "../images/logo-alt.png"
 import avatar from "../images/avatar.png"
+import classes from "../styles/Layout.module.css"
 
 export function Layout(props) {
   return (
     <div>
-      <header>
+      <header className={classes.header}>
         <img src={logo} alt="Todo daily logo" />
 
-        <div>
+        <div className={classes.avatar_wrapper}>
           <p>James</p>
           <img src={avatar} alt="Profile avatar" />
         </div>
