912ea0bc3523b344435e772cda686c9005828f86
Michael Peter <michaelhpet@gmail.com>
2023-04-18 21:00:30 +0100

    feat: add styles for Login page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import "./index.css"
+import "./styles/index.css"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import { Home } from "./pages/Home"
 import { Login } from "./pages/Login"
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,32 @@
 import { AuthButton } from "../components/AuthButton"
+import { Link } from "react-router-dom"
+import classes from "../styles/Login.module.css"
 import logo from "../images/logo.png"
+import googleIcon from "../images/google-icon.svg"
+import githubIcon from "../images/github-icon.svg"
 
 export function Login() {
   return (
-    <main>
-      <section>
-        <img src={logo} alt="Todo daily logo" />
+    <main className={classes.main}>
+      <section className={classes.form_wrapper}>
+        <Link to="/">
+          <img className={classes.logo_link} src={logo} alt="Todo daily logo" />
+        </Link>
+
         <h3>Signin</h3>
-        <AuthButton>Continue using Google</AuthButton>
-        <AuthButton>Continue using GitHub</AuthButton>
+
+        <div className={classes.auth_buttons}>
+          <AuthButton icon={googleIcon}>Continue using Google</AuthButton>
+          <AuthButton icon={githubIcon}>Continue using GitHub</AuthButton>
+        </div>
+
         <footer>
-          <p>Not have account? Signup</p>
+          <p>
+            <span>Not have account? </span>
+            <Link to="/signup">
+              <a>Signup</a>
+            </Link>
+          </p>
         </footer>
       </section>
     </main>
