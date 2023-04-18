78e6d5ff39a27e6f3145619746c0591b3298f09e
Michael Peter <michaelhpet@gmail.com>
2023-04-18 21:06:02 +0100

    feat: implement Signup page

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,3 +1,34 @@
+import { AuthButton } from "../components/AuthButton"
+import { Link } from "react-router-dom"
+import classes from "../styles/Login.module.css"
+import logo from "../images/logo.png"
+import googleIcon from "../images/google-icon.svg"
+import githubIcon from "../images/github-icon.svg"
+
 export function Signup() {
-  return <h1>Hello SIGNUP page here!</h1>
+  return (
+    <main className={classes.main}>
+      <section className={classes.form_wrapper}>
+        <Link to="/">
+          <img className={classes.logo_link} src={logo} alt="Todo daily logo" />
+        </Link>
+
+        <h3>Signup</h3>
+
+        <div className={classes.auth_buttons}>
+          <AuthButton icon={googleIcon}>Continue using Google</AuthButton>
+          <AuthButton icon={githubIcon}>Continue using GitHub</AuthButton>
+        </div>
+
+        <footer>
+          <p>
+            <span>Already have an account? </span>
+            <Link to="/login">
+              <a>Signin</a>
+            </Link>
+          </p>
+        </footer>
+      </section>
+    </main>
+  )
 }
