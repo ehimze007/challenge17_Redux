1d270927f08e7de268e9385f71d7c7d57d1de255
Michael Peter <michaelhpet@gmail.com>
2023-04-26 21:30:27 +0100

    feat: complete Upcoming page

--- src/pages/Upcoming.jsx
+++ src/pages/Upcoming.jsx
@@ -1,4 +1,5 @@
 import { Layout } from "../components/Layout"
+import { TodoItem } from "../components/TodoItem"
 import classes from "../styles/Dashboard.module.css"
 
 export function Upcoming() {
@@ -6,7 +7,37 @@ export function Upcoming() {
     <Layout>
       <main className={classes.main}>
         <h1 className={classes.page_title}>Upcoming</h1>
-        <p className={classes.page_subtitle}>January 2022</p>
+
+        <div className={classes.calendar_wrapper}>
+          <p>January 2022</p>
+
+          <div className={classes.days_wrapper}>
+            {Array(12)
+              .fill(0)
+              .map((number, index) => (
+                <div className={index === 0 ? classes.current_day : ""}>
+                  <p>{index + 1}</p>
+                </div>
+              ))}
+          </div>
+        </div>
+
+        <p className={classes.page_subtitle}>4/6 completed</p>
+
+        <section className={classes.todo_list}>
+          <TodoItem
+            title="Cooking a salmon sushi"
+            subtitle="Salmon and tuna i think is good for dinner, i wanna make it today :D"
+          />
+          <TodoItem
+            title="Write code for fizz buzz algorithm"
+            subtitle="Salmon and tuna i think is good for dinner, i wanna make it today :D"
+          />
+          <TodoItem
+            title="Build Todo Daily app"
+            subtitle="Salmon and tuna i think is good for dinner, i wanna make it today :D"
+          />
+        </section>
       </main>
     </Layout>
   )
