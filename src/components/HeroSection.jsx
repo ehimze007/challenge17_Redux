2f147305a5d34159664a7ff78ac50693e74fe97a
Michael Peter <michaelhpet@gmail.com>
2023-04-27 21:00:10 +0100

    refactor: link Hero button to login page

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,13 +1,16 @@
 import classes from "../styles/HeroSection.module.css"
 import { Button } from "./Button"
 import heroImage from "../images/hero-image.png"
+import { Link } from "react-router-dom"
 
 export function HeroSection() {
   return (
     <section className={classes.hero}>
       <article className={classes.hero_article}>
         <h1>Organizing your day activity with Todo Daily</h1>
-        <Button>Get started</Button>
+        <Link to="/login">
+          <Button>Get started</Button>
+        </Link>
       </article>
 
       <img src={heroImage} alt="Man at dock" />
