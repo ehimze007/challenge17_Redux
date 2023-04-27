e29a2c13913d3b3cc42ee9befd51a57a69bf7d0b
Michael Peter <michaelhpet@gmail.com>
2023-04-27 20:19:52 +0100

    feat: create markup for Profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,24 @@
 import { Layout } from "../components/Layout"
+import { Button } from "../components/Button"
 import classes from "../styles/Profile.module.css"
 
 export function Profile() {
   return (
     <Layout>
-      <main className={classes.main}></main>
+      <main className={classes.main}>
+        <form>
+          <input type="file" accept="image/*" />
+          <input type="text" placeholder="Enter your name" />
+          <input type="text" placeholder="Enter your username" />
+
+          <div>
+            <Button size="sm">Apply</Button>
+            <Button size="sm" variant="outlined">
+              Cancel
+            </Button>
+          </div>
+        </form>
+      </main>
     </Layout>
   )
 }
